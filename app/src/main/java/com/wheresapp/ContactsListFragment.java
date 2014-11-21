8072e404fc423670b1b941828bc286747610d596
palomagc <pgcescorpio@msn.com>
2014-11-21 02:37:34 +0100

    Funciono!
    Tuve que comentar dos líneas de dependencias en app/build.gradle para
    que me funcionase, ya veré más adelante a que se debe, quizás a los
    plugin esos

--- app/src/main/java/com/wheresapp/ContactsListFragment.java
+++ app/src/main/java/com/wheresapp/ContactsListFragment.java
@@ -47,26 +47,23 @@ public class ContactsListFragment extends ListFragment
     GoogleCloudMessaging gcm;*/
     String userNumber;
 
-    private class MySimpleCursorAdapter extends SimpleCursorAdapter {
+    private class CustomViewBinder implements SimpleCursorAdapter.ViewBinder {
 
-        public MySimpleCursorAdapter(Context context, int layout, Cursor c, String[] from,
-                                     int[] to, int flags) {
-            super(context, layout, c, from, to, flags);
-        }
-
-        public void bindView(View v, Context context, Cursor c) {
-            String src = c.getString(c.getColumnIndex(Contacts.PHOTO_URI));
-            String name = c.getString(c.getColumnIndex(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                    Contacts.DISPLAY_NAME_PRIMARY : Contacts.DISPLAY_NAME));
-
-            ImageView im = (ImageView) v.findViewById(android.R.id.icon);
-            if (src != null) {
-                im.setImageURI(Uri.parse(src));
-            } else {
-                im.setImageResource(R.drawable.ic_action_person);
+        @Override
+        public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+            if (columnIndex == cursor.getColumnIndex(Contacts.PHOTO_URI)) {
+                String src = cursor.getString(columnIndex);
+                ImageView im = (ImageView) view.findViewById(android.R.id.icon);
+                if (src != null) {
+                    im.setImageURI(Uri.parse(src));
+                } else {
+                    im.setImageResource(R.drawable.ic_action_person);
+                }
+                return true;
             }
-            TextView text = (TextView) v.findViewById(android.R.id.text1);
-            text.setText(name);
+            // For others, we simply return false so that the default binding
+            // happens.
+            return false;
         }
     }
 
@@ -112,11 +109,12 @@ public class ContactsListFragment extends ListFragment
         /*Resources resources = this.getResources();
         int iconId = R.drawable.ic_action_person;
          */// Create an empty adapter we will use to display the loaded data.
-        mAdapter = new MySimpleCursorAdapter(getActivity(), R.layout.contacts_list_item, null,
+        mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.contacts_list_item, null,
                 new String[] { Contacts.PHOTO_URI,
                         Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
                                 Contacts.DISPLAY_NAME_PRIMARY : Contacts.DISPLAY_NAME },
                 new int[] {android.R.id.icon, android.R.id.text1 }, 0);
+        mAdapter.setViewBinder(new CustomViewBinder());
         setListAdapter(mAdapter);
 
         // Prepare the loader.  Either re-connect with an existing one,
