f6557c89cc491061e751fcfcaed2df3725efa868
palomagc <pgcescorpio@msn.com>
2014-11-29 22:23:16 +0100

    Solo rellene la clase para entidad llamada

--- app/src/main/java/com/wheresapp/modelTEMP/Call.java
+++ app/src/main/java/com/wheresapp/modelTEMP/Call.java
@@ -1,9 +1,57 @@
 package com.wheresapp.modelTEMP;
 
 import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.google.api.client.util.DateTime;
 
 /**
  * Created by Victorma on 25/11/2014.
  */
+@Table(name = "Calls")
 public class Call extends Model{
+
+    @Column(name = "Start")
+    private DateTime start;
+
+    @Column(name = "End")
+    private DateTime end;
+
+    @Column(name = "Receiver")
+    private String receiver;
+
+    @Column(name = "State")
+    private Integer state;
+
+    public DateTime getStart() {
+        return start;
+    }
+
+    public void setStart(DateTime lastSeen) {
+        this.start = start;
+    }
+
+    public DateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(DateTime end) {
+        this.end = end;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
 }
